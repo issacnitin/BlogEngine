3d55abb79bafc5c564b7f747b082940129fe4042
Nitin Issac Joy <nitin.i.joy@gmail.com>
2018-12-28 23:34:51 +0530

    Basic blog post

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+class App extends React.Component <{}, { posts: string}>{
 
-class App extends React.Component {
+  // let blogposts: { [key:string]:string } = {} as { [key:string]:string };
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      posts: ""
+    };
+
+    const fs = require('fs');
+    const thisObject = this;
+    fs.readdir('posts', (err: any, filenames: any) => {
+      if(err) { return; }
+      filenames.forEach((filename: any) => {
+        fs.readfile('posts/' + filename, (err2: any, content: any) => {
+          if(err2) { return; }
+          thisObject.setState({
+            posts: content
+          });
+        });
+      });
+    })
+
+  }
+
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      {this.state.posts}
       </div>
     );
   }
