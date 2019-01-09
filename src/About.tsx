26cb91887a8c6446bbdafd82d0eae9c0e65e9456
Nitin Issac Joy <nitin.i.joy@gmail.com>
2019-01-09 22:39:55 +0530

    Updating About Page

--- src/About.tsx
+++ src/About.tsx
@@ -5,7 +5,10 @@ class About extends React.Component <{}, { posts: string[]}>{
 
     public render() {
 
-        let about = "Nitin Issac Joy, 25\nnitin.i.joy@gmail.com".split('\n').map((item, i) => <p key={i}>{item}</p>);;
+        let about = "Nitin Issac Joy, 25\n \
+                    nitin.i.joy@gmail.com\n \
+                    Feel free to contribute to the blog\n \
+                    https://github.com/issacnitin/BlogEngine\n".split('\n').map((item, i) => <p key={i}>{item}</p>);;
         return (
             <div className='About'>
                 {about}
